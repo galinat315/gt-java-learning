b219d36e150b8c535713870f6788366b4cf81681
Galina <galina.tofan315@gmail.com>
2019-07-25 01:17:18 +0400

    Hello

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello world. Why am I such a potato?");
+    }
+}
